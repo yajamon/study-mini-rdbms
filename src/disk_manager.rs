9086be11f5d5a371164cf4c2ef701ce502a7d4ef
yajamon <yajamon.tatsuki@gmail.com>
2021-05-05 23:41:11 +0900

    メソッドの定義だけを先行して書く
    - `// （省略）`とされた部分は `unimplementated!()`とした
    - `io::Result` が唐突
    - `PageId` が唐突
        - どうしたいかわからないので `type` でお茶濁す
    - `std::path::Path` が唐突
    - `: impl AsRef<Path>` 書いたことないのでわからん
    - "パニックにならないでください！"は Rust 触ってる人にしか通じなさそう

--- src/disk_manager.rs
+++ src/disk_manager.rs
@@ -1,8 +1,39 @@
 use std::fs::File;
+use std::io;
+use std::path::Path;
 
 pub struct DiskManager {
     // ヒープファイルのファイルディスクリプタ
     heap_file: File,
     // 採番するページIDを決めるカウンタ
     next_page_id: u64,
+}
+
+type PageId = u64;
+
+impl DiskManager {
+    /// コンストラクタ
+    pub fn new(data_file: File) -> io::Result<Self> {
+        unimplemented!();
+    }
+
+    /// ファイルパスを指定して開く
+    pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        unimplemented!();
+    }
+
+    /// 新しいページIDを採番する
+    pub fn allocate_page(&mut self) -> PageId {
+        unimplemented!();
+    }
+
+    /// ページのデータを読み出す
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        unimplemented!();
+    }
+
+    /// データをページに書き出す
+    pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
+        unimplemented!();
+    }
 }
