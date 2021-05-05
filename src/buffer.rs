f6866333e8d3cdacafc206a48bbfa9839824742a
yajamon <yajamon.tatsuki@gmail.com>
2021-05-06 01:58:19 +0900

    BufferPool::evict() に Clock-Sweep アルゴリズムを実装する
    - 例によって BufferId は newType patternだった
    - 唐突の `size()`
        - おそらく buffers の長さ（カンニングした）
        - ヒントが薄すぎる
            - このアルゴリズムはすべてのBufferを巡回する
            - BufferPool::buffers は `Vec<Frame>`
            - 掴めなかった Buffer の数だけインクリメントする
    - 唐突の インデックスアクセス
        - `Index`, `IndexMut` の実装を要する

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,10 +1,12 @@
 use std::cell::{Cell, RefCell};
 use std::collections::HashMap;
+use std::ops::{Index, IndexMut};
 use std::rc::Rc;
 
 use crate::disk_manager::*;
 
-type BufferId = u64;
+#[derive(Clone, Copy)]
+pub struct BufferId(u64);
 
 pub type Page = [u8; PAGE_SIZE];
 
@@ -22,6 +24,54 @@ pub struct Frame {
 pub struct BufferPool {
     buffers: Vec<Frame>,
     next_victim_id: BufferId,
+}
+impl BufferPool {
+    fn size(&self) -> u64 {
+        todo!()
+    }
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive_pinned = 0;
+
+        // 捨てて良い BufferId を探す
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                // 掴めるということは使用していない
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                // 掴めない
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        BufferId((buffer_id.0 + 1) % self.size())
+    }
+}
+
+impl Index<BufferId> for BufferPool {
+    type Output = Frame;
+
+    fn index(&self, index: BufferId) -> &Self::Output {
+        todo!()
+    }
+}
+
+impl IndexMut<BufferId> for BufferPool {
+    fn index_mut(&mut self, index: BufferId) -> &mut Self::Output {
+        todo!()
+    }
 }
 
 pub struct BufferPoolManager {
